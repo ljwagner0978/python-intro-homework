0310bae099741eb3f27aa728c646034e8d57ff76
ljwagner0978 <lswagner24@gmail.com>
2026-07-17 10:44:11 -0400

    Update warmup3.py

--- week-2/assignment-2/warmup3.py
+++ week-2/assignment-2/warmup3.py
@@ -2,3 +2,4 @@
 #3fa7b0e (HEAD -> main, origin/main) ok trying to edit and make correct
 #d9bf16b Ok check
 
+print("This week, I learned all about the Git system and how it works through trial and error.")
