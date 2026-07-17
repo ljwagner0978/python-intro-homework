20d108071fbb3172f56003a4fe6ce6a91af334ef
ljwagner0978 <lswagner24@gmail.com>
2026-07-17 10:51:34 -0400

    Update mini_project.py

--- week-2/assignment-2/mini_project.py
+++ week-2/assignment-2/mini_project.py
@@ -4,7 +4,7 @@ while True:
  fahrenheit = (input("Enter a temperature in Farenheit: "))
  try:
   celsius = round(((float(fahrenheit) - 32) * 5 / 9), 1)
-  print(f"{round(float(fahrenheit),1)} °F is {celsius} °C.")
+  print(f"{round(float(fahrenheit),1)}°F is {celsius}°C.")
   break
  except ValueError:
     print("Oops!  That was not a valid number.  Try again...")
