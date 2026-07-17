47310d2018021ce6f9fe5d8868e96110310010a3
ljwagner0978 <lswagner24@gmail.com>
2026-07-17 10:59:52 -0400

    Update mini_project.py

--- week-2/assignment-2/mini_project.py
+++ week-2/assignment-2/mini_project.py
@@ -1,7 +1,7 @@
 #This code takes the user's inputted fahrenheit temperature value and converts it to celsius
 
 while True:
- fahrenheit = (input("Enter a temperature in Farenheit: "))
+ fahrenheit = (input("Enter a temperature in Fahrenheit: "))
  try:
   celsius = round(((float(fahrenheit) - 32) * 5 / 9), 1)
   print(f"{round(float(fahrenheit),1)}°F is {celsius}°C.")
