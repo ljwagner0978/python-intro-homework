fc6c720c10da4df69b5749e3307c1b0d9ead33f6
ljwagner0978 <lswagner24@gmail.com>
2026-07-17 09:15:05 -0400

    Add files via upload

---
+++ week-2/assignment-2/warmup2.py
@@ -0,0 +1,18 @@
+#This code takes the user's inputted date value and reformats the date into a sentence structure.
+
+# Navigation commands I used:
+# cd lswag/python_homework/python-intro-homework
+# ls
+# cd week-2/assignment-2
+
+import datetime as dt
+
+while True:
+    x = input("What is today's date? (MM/DD/YYYY format) ")
+    try:
+        user_date = dt.date.strptime(x, "%m/%d/%Y")
+        print(f'You said today is {user_date.strftime("%B")} {user_date.strftime("%d")}, {user_date.strftime("%Y")}.')
+        break
+    except ValueError:
+        print("Oops!  That was not a valid date.  Try again...")
+
