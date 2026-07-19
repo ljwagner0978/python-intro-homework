bb3e3257585438e7c7cf66c0602bd4d127eb40f2
ljwagner0978 <lswagner24@gmail.com>
2026-07-19 12:30:12 -0400

    Update warmup3.py

--- week-2/assignment-2/warmup3.py
+++ week-2/assignment-2/warmup3.py
@@ -1,4 +1,4 @@
 # git log --oneline output:
-# 294742d Add warmup1 and warmup4 for week 2
+# c49dcba Edit/Completion of week 2 assignments
 
 print("This week, I learned all about the Git system and how it works through trial and error.")
