ced92bbe18140482c0ec608ded0d22fbea0bcafb
ljwagner0978 <lswagner24@gmail.com>
2026-07-19 11:43:19 -0400

    Update warmup4.py for Airhub feedback

--- week-3/assignment-3/warmup4.py
+++ week-3/assignment-3/warmup4.py
@@ -2,12 +2,12 @@
 
 num = int(input("Enter a number: "))
 
-if num == 0:
-    print("0 is zero.")
-elif num > 0:
+if num > 0:
     print(f"{num} is positive.")
 elif num < 0:
     print(f"{num} is negative.")
+else:
+    print(f"{num} is zero.")
 
 if (num % 2 == 0) or num == 0: 
    print(f"{num} is even.")
