0771797749fd14fca2340e5f774feede10f99f9c
ljwagner0978 <lswagner24@gmail.com>
2026-07-19 10:11:19 -0400

    Updated incorrect code logic

--- warmup2.py
+++ warmup2.py
@@ -2,7 +2,7 @@
 
 age = int(input("Enter your age: "))
 
-if age == 0 and age <= 12:
+if age >= 0 and age <= 12:
     print("You are a Child.")
 elif age >= 13 and age <= 17:
     print("You are a Teen.")
