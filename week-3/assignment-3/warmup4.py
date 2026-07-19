c7f77ca08ac1254e643b70f813e01c0c31824082
ljwagner0978 <lswagner24@gmail.com>
2026-07-19 11:03:37 -0400

    Create warmup4.py

---
+++ week-3/assignment-3/warmup4.py
@@ -0,0 +1,18 @@
+#This code takes a user's inputted number value and evaluates if it is positive/negative and even/odd.
+
+num = int(input("Enter a number: "))
+
+if num == 0:
+    print("0 is zero.")
+elif num > 0:
+    print(f"{num} is positive.")
+elif num < 0:
+    print(f"{num} is negative.")
+
+if num == 0:
+   print("0 is even.")
+elif (num % 2 == 0): 
+   print(f"{num} is even.")
+elif (num % 2 != 0):
+   print(f"{num} is odd.")
+
