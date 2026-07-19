235824a45f8c8e7886de5771605330f4bf1c7e07
ljwagner0978 <lswagner24@gmail.com>
2026-07-19 10:25:24 -0400

    Made code Airhub friendly

--- mini_project.py
+++ mini_project.py
@@ -1,45 +1,32 @@
 #This code takes the user's inputted day and time of day value and prints a suggested activity depending on the day and time of day
 
-while True:
- day = (input("What day is it?: ")).capitalize()
- phase = (input("What time of day?: ")).capitalize()
- 
- try:
-  
-  days_of_week_array = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-  time_of_day_array = ["Morning", "Afternoon", "Evening"]
 
-  if day not in ("Monday","Tuesday","Wednesday","Thursday", "Friday", "Saturday", "Sunday"):
-    print("Sorry, I don't recognize that day. Try: Monday, Tuesday, Wednesday...")
-    break
-  if phase not in ("Morning", "Afternoon", "Evening"):
-    print("Sorry, I don't recognize that time. Try: Morning, Afternoon, or Evening")
-    break
-  
-  a = ""
-  if day in ("Monday","Tuesday","Wednesday") and phase == "Morning":
-    a = ("It's morning time! Get to work!")
-  elif day == "Thursday" and phase == "Morning":
-    a = ("Its Thursday morning, definitely skip work and all other obligations.")
-  elif day == "Friday" and phase == "Morning":
-    a = ("Its Friday morning, weekend coming up!. Fantasize about your weekend plans.")
-  elif day in ("Saturday","Sunday") and phase == "Morning": 
-    a = ("Its the weekend! Go for a stroll or have a hot cup of Joe.")
-  elif day in ("Monday","Tuesday","Wednesday","Thursday") and phase == "Afternoon":
-    a = ("Afternoon time! Play games with friends or go to the gym. Rest and relax.")
-  elif day == "Friday" and phase == "Afternoon":
-    a = ("Weekend time! Go splurge on some dinner, you deserve it!")
-  elif day in ("Saturday", "Sunday") and phase == "Afternoon":
-    a = ("The world is your oyster. Go travel, hang out with old friends and make new ones!")
-  elif day in ("Saturday", "Sunday", "Friday") and phase == "Evening":
-    a = ("Perfect night for a movie or trying a new recipe.")
-  elif day in ("Monday","Tuesday","Wednesday","Thursday") and phase == "Evening":
-    a = ("Read or book, doomscroll, or prep for bed. You got work tomorrow.")
+ day = (input("What day is it?: ")).strip.lower()
+ time = (input("What time of day?: ")).strip.lower()
+
+ if day not in ("monday","tuesday","wednesday","thursday", "friday", "saturday", "sunday"):
+   print("Sorry, I don't recognize that day. Try: Monday, Tuesday, Wednesday...")
+ elif time not in ("morning", "afternoon", "evening"):
+   print("Sorry, I don't recognize that time. Try: morning, afternoon, or evening")
+ elif day in ("monday","tuesday","wednesday") and time == "morning":
+   print("Suggestion: It's morning time! Get to work!")
+  elif day == "thursday" and time == "morning":
+   print("Suggestion: Its thursday morning, definitely skip work and all other obligations.")
+  elif day == "friday" and time == "morning":
+   print("Suggestion: Its friday morning, weekend coming up!. Fantasize about your weekend plans.")
+  elif day in ("saturday","sunday") and time == "morning": 
+   print("Suggestion: Its the weekend! Go for a stroll or have a hot cup of Joe.")
+  elif day in ("monday","tuesday","wednesday","thursday") and time == "afternoon":
+   print("Suggestion: Afternoon time! Play games with friends or go to the gym. Rest and relax.")
+  elif day == "friday" and time == "afternoon":
+   print("Suggestion: Weekend time! Go splurge on some dinner, you deserve it!")
+  elif day in ("saturday", "sunday") and time == "afternoon":
+   print("Suggestion: The world is your oyster. Go travel, hang out with old friends and make new ones!")
+  elif day in ("saturday", "sunday", "friday") and time == "evening":
+   print("Suggestion: Perfect night for a movie or trying a new recipe.")
+  elif day in ("monday","tuesday","wednesday") and time == "evening":
+   print("Suggestion: Read or book, doomscroll, or prep for bed. You got work tomorrow.")
+  elif day == "thursday" and time == "evening":
+   print("Suggestion: Have some cake! Tomorrow's Friday!")
   else:
-    print("Sorry, I dont recognize that time.")
-    a = ("Incorrect phase value inputted")
-
-  print(f"Suggestion: {a}")
-  break
- except ValueError:
-    print("Sorry, I don't recognize that day or time. Please try again.")
+   print("Suggestion: Redo this!")
