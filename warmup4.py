f0e7120258349cb03f48cdc7b572d3e90d765cf6
ljwagner0978 <lswagner24@gmail.com>
2026-07-19 10:13:19 -0400

    Updated with Airbud friendly code

--- warmup4.py
+++ warmup4.py
@@ -4,17 +4,21 @@ num = int(input("Enter a number: "))
 
 if num == 0:
     print("0 is zero.")
-elif abs(num) != num:
-    print(f"{num} is negative.")
-else:
-    print(f"{num} is positive.")
-
-if num == 0:
     print("0 is even.")
-elif ((num / 2) % 1 != 0):
+elif num < 0 and (num % 2 == 0):
+    print(f"{num} is negative.")
+    print(f"{num} is even.")
+elif num > 0 and (num % 2 == 0):
+    print(f"{num} is positive.")
+    print(f"{num} is even.")
+elif num < 0 and (num % 2 != 0):
+    print(f"{num} is negative.")
+    print(f"{num} is odd.")
+elif num > 0 and (num % 2 != 0):
+    print(f"{num} is positive.")
     print(f"{num} is odd.")
 else:
-    print(f"{num} is even.")
+    print(f"{num} is not a valid number")
 
 
   
