661132ef557e2a46805ff50099ed57a2a9d75c35
ljwagner0978 <lswagner24@gmail.com>
2026-07-19 11:52:43 -0400

    Update warmup2.py to be Airhub friendly

--- week-2/assignment-2/warmup2.py
+++ week-2/assignment-2/warmup2.py
@@ -5,14 +5,6 @@
 # ls
 # cd week-2/assignment-2
 
-import datetime as dt
-
-while True:
-    x = input("What is today's date? (MM/DD/YYYY format) ")
-    try:
-        user_date = dt.date.strptime(x, "%m/%d/%Y")
-        print(f'You said today is {user_date.strftime("%B")} {user_date.strftime("%d")}, {user_date.strftime("%Y")}.')
-        break
-    except ValueError:
-        print("Oops!  That was not a valid date.  Try again...")
+    x = input("What is today's date?")
+    print(f'You said today is {x}.')
 
