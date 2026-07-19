e06fa615815786e308fa94f89634e80b2cdbb14b
ljwagner0978 <lswagner24@gmail.com>
2026-07-19 12:28:12 -0400

    Update warmup2.py

--- week-2/assignment-2/warmup2.py
+++ week-2/assignment-2/warmup2.py
@@ -1,10 +1,9 @@
-#This code takes the user's inputted date value and reformats the date into a sentence structure.
 
 # Navigation commands I used:
-# cd lswag/python_homework/python-intro-homework
+# cd python-intro-homework
 # cd week-2
 # cd assignment-2
 
-x = input("What is today's date?: ")
+x = input("What is today's date? ")
 print(f'You said today is {x}.')
 
