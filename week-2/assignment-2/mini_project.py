a1fefb659082b1d71857434cfc0bf6d74f1725d5
ljwagner0978 <lswagner24@gmail.com>
2026-07-19 12:09:48 -0400

    Updated mini_project.py with Airhub feedback

--- week-2/assignment-2/mini_project.py
+++ week-2/assignment-2/mini_project.py
@@ -1,10 +1,6 @@
 #This code takes the user's inputted fahrenheit temperature value and converts it to celsius
 
-while True:
- fahrenheit = (input("Enter a temperature in Fahrenheit: "))
- try:
-  celsius = round(((float(fahrenheit) - 32) * 5 / 9), 1)
-  print(f"{round(float(fahrenheit),1)}°F is {celsius}°C.")
-  break
- except ValueError:
-    print("Oops!  That was not a valid number.  Try again...")
+ fahrenheit = float(input("Enter a temperature in Fahrenheit: "))
+ 
+celsius = round((fahrenheit - 32) * 5 / 9), 1)
+  print(f"{round(fahrenheit,1)}°F is {celsius}°C.")
