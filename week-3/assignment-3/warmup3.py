2912ef2eac2ab1b4c15421c22ce7e0cd39a02396
ljwagner0978 <lswagner24@gmail.com>
2026-07-19 11:02:45 -0400

    Create warmup3.py

---
+++ week-3/assignment-3/warmup3.py
@@ -0,0 +1,7 @@
+#This code is an evaluation of True and False statements and why they are True or False
+
+print(not True and False) #not True is False; False is also False so it is False.
+print(True or False and False) #True is True; False and False is False. Only one value needs to be True so it is True.
+print(not (5 > 3)) #not keyword makes the evaluated expression False so it is False.
+print(10 == 10 and 4 != 4) #10 equals 10 is True; 4 does not equal 4 is False. Both must be True so it is False.
+print(not False or not True) #not False is True; not True is False. Only one value needs to be True so it is True.
