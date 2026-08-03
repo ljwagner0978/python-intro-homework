ed0324fbb84668e7ef217a6ff05f33cae351a2e6
ljwagner0978 <lswagner24@gmail.com>
2026-08-03 14:00:02 -0400

    Update mini_project.py with Airhub changes

--- week-5/assignment-5/mini_project.py
+++ week-5/assignment-5/mini_project.py
@@ -8,8 +8,7 @@ print("4. Sort the list")
 print("5. Quit")
 user_input = input("Choose an option (1-5): ")
 
-while True:
-    if user_input.isdigit() == False or int(user_input) <= 0 or int(user_input) >= 6:
+while user_input.isdigit() == False or int(user_input) <= 0 or int(user_input) >= 6:
         print("Invalid input, try again please.")
         print("=== Number Cruncher ===")
         print("1. Find minimum")
@@ -18,71 +17,73 @@ while True:
         print("4. Sort the list")
         print("5. Quit")
         user_input = input("Choose an option (1-5): ")
-    elif int(user_input) == 1:
-        min_number = numbers[0]
-        for number in numbers:
-            if(min_number > number):
-                min_number = number
-        print(min_number)
-        print("=== Number Cruncher ===")
-        print("1. Find minimum")
-        print("2. Find maximum")
-        print("3. Search for a number")
-        print("4. Sort the list")
-        print("5. Quit")
-        user_input = input("Choose an option (1-5): ")
-    elif int(user_input) == 2:
-        top_number = numbers[0]
-        for number in numbers:
-            if(top_number < number):
-                top_number = number
-        print(top_number)
-        print("=== Number Cruncher ===")
-        print("1. Find minimum")
-        print("2. Find maximum")
-        print("3. Search for a number")
-        print("4. Sort the list")
-        print("5. Quit")
-        user_input = input("Choose an option (1-5): ")
-    elif int(user_input) == 3:
-        number_input = input("Please provide a number: ")
-        while True:
-            if number_input.isdigit() == False:
-               print("Invalid number inputted. Please try again")
-               number_input = input("Please provide a number: ")
-            for i in range(len(numbers)):
-                if numbers[i] == int(number_input):
-                   print(f"Found at index: {i}")
-                   break
-                elif i == (len(numbers)-1):
-                   print("Not found")
-                   break
-            break
-        print("=== Number Cruncher ===")
-        print("1. Find minimum")
-        print("2. Find maximum")
-        print("3. Search for a number")
-        print("4. Sort the list")
-        print("5. Quit")
-        user_input = input("Choose an option (1-5): ")
-    elif int(user_input) == 4:
-        swapped = True
-        while swapped != False:
-            for i in range(len(numbers)-1):
-                for j in range(len(numbers)-i-1):
-                    if numbers[j] > numbers[j+1]:
-                        numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-                        swapped = True
-                    else:
-                        swapped = False
-        print(numbers)
-        print("=== Number Cruncher ===")
-        print("1. Find minimum")
-        print("2. Find maximum")
-        print("3. Search for a number")
-        print("4. Sort the list")
-        print("5. Quit")
-        user_input = input("Choose an option (1-5): ")
-    elif int(user_input) == 5:
-        print("Goodbye.")
-        break
+
+while int(user_input) >= 1 and int(user_input) <= 5:
+        if int(user_input) == 1:
+            min_number = numbers[0]
+            for number in numbers:
+                if(min_number > number):
+                    min_number = number
+            print(min_number)
+            print("=== Number Cruncher ===")
+            print("1. Find minimum")
+            print("2. Find maximum")
+            print("3. Search for a number")
+            print("4. Sort the list")
+            print("5. Quit")
+            user_input = input("Choose an option (1-5): ")
+        elif int(user_input) == 2:
+            top_number = numbers[0]
+            for number in numbers:
+                if(top_number < number):
+                    top_number = number
+            print(top_number)
+            print("=== Number Cruncher ===")
+            print("1. Find minimum")
+            print("2. Find maximum")
+            print("3. Search for a number")
+            print("4. Sort the list")
+            print("5. Quit")
+            user_input = input("Choose an option (1-5): ")
+        elif int(user_input) == 3:
+            number_input = input("Please provide a number: ")
+            while True:
+                if number_input.isdigit() == False:
+                   print("Invalid number inputted. Please try again")
+                   number_input = input("Please provide a number: ")
+                for i in range(len(numbers)):
+                    if numbers[i] == int(number_input):
+                       print(f"Found at index: {i}")
+                       break
+                    elif i == (len(numbers)-1):
+                       print("Not found")
+                       break
+                break
+            print("=== Number Cruncher ===")
+            print("1. Find minimum")
+            print("2. Find maximum")
+            print("3. Search for a number")
+            print("4. Sort the list")
+            print("5. Quit")
+            user_input = input("Choose an option (1-5): ")
+        elif int(user_input) == 4:
+            swapped = True
+            j = 0
+            while swapped != False:
+                
+                if numbers[j] > numbers[j+1]:
+                    numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+                    swapped = True
+                else:
+                    swapped = False
+                j+= 1
+            print(numbers)
+            print("=== Number Cruncher ===")
+            print("1. Find minimum")
+            print("2. Find maximum")
+            print("3. Search for a number")
+            print("4. Sort the list")
+            print("5. Quit")
+            user_input = input("Choose an option (1-5): ")
+        elif int(user_input) == 5:
+            print("Goodbye.")
