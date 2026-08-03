63000e24af055a9652febd2cea09dc532339eb50
ljwagner0978 <lswagner24@gmail.com>
2026-08-03 12:02:24 -0400

    Update with feedback from Airhub

--- week-5/assignment-5/warmup2.py
+++ week-5/assignment-5/warmup2.py
@@ -1,10 +1,5 @@
-while True:
- try:
-  x = int(input("Enter a positive integer: "))
-  while x < 0:
+x = input("Enter a positive integer: ")
+while x.isdigit() == False or int(x) < 1:
    print("That's not a positive integer. Try again.")
-   x = int(input("Enter a positive integer: "))
-  print(f"Got it: {x}")
-  break
- except ValueError:
-    print("That's not a positive integer. Try again.")
+   x = input("Enter a positive integer: ")
+print(f"Got it: {x}")
