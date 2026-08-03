f5999e8b7f983edd2e278b0a22c95b7e29b5ac43
ljwagner0978 <lswagner24@gmail.com>
2026-08-03 13:15:02 -0400

    More Airhub Feedback

--- week-5/assignment-5/warmup3.py
+++ week-5/assignment-5/warmup3.py
@@ -1,10 +1,12 @@
 names = ["Alex", "George", "Mary", "Juan", "Andre", "Michelle", "Lisa", "Jennifer"]
 x = input("Enter a name to search for: ")
 index = 0
+match_found = False
 for name in names:
     if x == name:
        print(f'Found "{x}" at index {index}.')
+       match_found = True
        break
-    elif index == len(names)-1:
-        print(f'"{x}" was not found in the list.')
     index += 1
+if match_found == False:
+    print(f'"{x}" was not found in the list.')
