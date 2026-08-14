4db306e1f797137d5c61b892f3ba3358eeed8b1d
ljwagner0978 <lswagner24@gmail.com>
2026-08-13 22:21:44 -0400

    Update warmup3.py

--- week-6/warmup3.py
+++ week-6/warmup3.py
@@ -1,7 +1,7 @@
 def celsius_to_fahrenheit(c):
     z = round((c * 9/5) + 32, 1)
     return(z)
-
+#print(z)
 #NameError: name 'z' is not defined   
 conversion = celsius_to_fahrenheit(32)
 print(conversion)
