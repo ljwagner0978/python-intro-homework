c290b59f892deb2e843d64c5a8fdcc15dfdc5514
ljwagner0978 <lswagner24@gmail.com>
2026-08-18 16:03:34 -0400

    Adding relevant Airhub edits

--- week-7/warmup1.py
+++ week-7/warmup1.py
@@ -1,3 +1,3 @@
-with open("C:/Users/lswag/python_class/python_homework/python-intro-homework/week-7/data/notes.txt", 'r') as file:
+with open("./data/notes.txt", 'r') as file:
     for i, line in enumerate(file):
-        print(f'Line {i+1}: {line}')
+        print(f'Line {i+1}: {line.strip()}')
--- week-7/warmup3.py
+++ week-7/warmup3.py
@@ -1,9 +1,9 @@
 import os
 path = os.getcwd()
 print(path)
-if os.path.exists("C:/Users/lswag/python_class/python_homework/python-intro-homework/week-7/data/expenses.csv"):
+if os.path.exists("./data/expenses.csv"):
     print("expenses.csv found.")
 else:
     print("expenses.csv not found.")  
-path = os.path.join(os.getcwd(), "data", "expenses.csv")
+path = os.path.join("data", "expenses.csv")
 print(path)
