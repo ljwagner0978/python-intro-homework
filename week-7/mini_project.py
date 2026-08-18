291b20de8d404b96ac7f48b4d8d9723836efbfbc
ljwagner0978 <lswagner24@gmail.com>
2026-08-18 15:51:31 -0400

    Adding all Assignment 7 deliverables

---
+++ week-7/mini_project.py
@@ -0,0 +1,27 @@
+import os
+import csv
+from datetime import datetime
+
+try:
+    if(os.path.exists("C:/Users/lswag/python_class/python_homework/python-intro-homework/week-7/data/expenses.csv")):
+        with open("C:/Users/lswag/python_class/python_homework/python-intro-homework/week-7/data/expenses.csv", 'r') as file:
+            reader = csv.DictReader(file)
+            itemlist = []
+            total = 0
+            user_input = input("What type of report would you like to choose? (Food, Transport, Utilities, or Entertainment): ")
+            while user_input not in ["Food", "Transport", "Utilities", "Entertainment"]:
+                print("Invalid input, try again please.")
+                user_input = input("What type of report would you like to view? (Food, Transport, Utilities, or Entertainment): ")
+            for row in reader:
+                itemlist.append({"date": row["date"],"category": row["category"],"description": row["description"],"amount": float(row["amount"])})
+                if(row["category"] == user_input): total += float(row["amount"])
+            new_itemlist = [item for item in itemlist if item['category'] == user_input]
+            report_name = user_input.lower() + "_report.txt"
+            with open(report_name, 'w') as file:
+                file.write(f"\n{user_input} Expense Report — generated {datetime.now().strftime("%B %d, %Y")}")
+                for item in new_itemlist:
+                    file.write(f'\n{item["date"]}: ${item["amount"]}')
+                file.write(f'\nTotal: ${round(total, 2)}')
+                print("Report successfully generated.")       
+except Exception as e:
+    print(f"An error occurred: {e}")
---
+++ week-7/warmup4.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+today = datetime.now()
+print(f'Today is {today.strftime("%B %d, %Y")}.')
