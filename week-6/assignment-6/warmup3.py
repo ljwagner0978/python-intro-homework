28d769c5e2a399fc07a45d920342bb86e7b022b5
ljwagner0978 <lswagner24@gmail.com>
2026-08-18 19:00:56 -0400

    Update warmup3.py

--- week-6/assignment-6/warmup3.py
+++ week-6/assignment-6/warmup3.py
@@ -2,10 +2,11 @@ def celsius_to_fahrenheit(c):
     z = round((c * 9/5) + 32, 1)
 #print(z)
 #NameError: name 'z' is not defined
+#The variable z is a local variable and cannot be accessed in global.
 
-def celsius_to_fahrenheit(c):
-    z = round((c * 9/5) + 32, 1)
-    return(z)
-conversion = celsius_to_fahrenheit(32)
-print(conversion)
+def givename(Name):
+    name = Name
+    return name
+print(givename("Sally"))
+#This works because this function returns the value of the local variable "name"
 
