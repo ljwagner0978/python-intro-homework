8cda84cddad72ff62643a56f3f932268238c5bba
ljwagner0978 <lswagner24@gmail.com>
2026-08-25 14:31:09 -0400

    Even more edits

--- week-8/assignment-8/mini_project.py
+++ week-8/assignment-8/mini_project.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 def go_thru(reader):
     data_list = []
@@ -11,34 +12,31 @@ def go_thru(reader):
         if None in row:
             skipped_rows_list.append(f'Row {i+1}: extra column detected — skipped')
             rows_skipped += 1
-        else:
-            try:
-                data_list.append({"name": row["name"],"category": row["category"], "amount": float(row["amount"])})
-                rows_parsed += 1
-            except ValueError as e:
-                skipped_rows_list.append(f'Row {i+1}: ValueError - {e}')
-                rows_skipped += 1
-            except KeyError as e:
-                skipped_rows_list.append(f'Row {i+1}: KeyError - {e}')
-                rows_skipped += 1
+        try:
+            data_list.append({"name": row["name"],"category": row["category"], "amount": float(row["amount"])})
+            rows_parsed += 1
+        except ValueError as e:
+            skipped_rows_list.append(f'Row {i+1}: ValueError — {e}')
+            rows_skipped += 1
+        except KeyError as e:
+            skipped_rows_list.append(f'Row {i+1}: KeyError — {e}')
+            rows_skipped += 1
     print("=== CSV Report ===")
     print(f"Rows attempted:  {rows_attempted}")
     print(f"Rows parsed:     {rows_parsed}")
-    print(f"Rows skipped:    {rows_skipped}")
-    print()
-    print()
+    print(f"Rows skipped:    {rows_skipped}\n\n")
     print("Skipped rows:")
     for row in skipped_rows_list:
-        print((' ' * 2) + f'{row}')
-    print()
-    print()
-    print("Clean data:")
+        print(f'  {row}')
+    print("\n\nClean data:")
     for row in data_list:
-        print((' ' * 2) + f'{row["name"]} | {row["category"]} | ${row["amount"]:,.2f}')
+        print(f'  {row["name"]} | {row["category"]} | ${row["amount"]:,.2f}')
 
-try:
-    with open("../data/messy_data.csv", 'r') as file:
-        reader = csv.DictReader(file)
-        go_thru(reader)                   
-except FileNotFoundError:
-    print(f"Error: 'messy_data.csv' was not found. Please check the file path and try again.")
+
+if os.path.exists("../data/messy_data.csv"):
+    try:
+        with open("../data/messy_data.csv", 'r') as file:
+            reader = csv.DictReader(file)
+            go_thru(reader)                   
+    except FileNotFoundError:
+            print(f"Error: 'messy_data.csv' was not found. Please check the file path and try again.")
