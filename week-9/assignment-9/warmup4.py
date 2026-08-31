d7032c88a23d6da6d6cb9fb0829a7271c92d2324
ljwagner0978 <lswagner24@gmail.com>
2026-08-31 19:35:39 -0400

    Finishing warmup4.py edits

--- week-9/assignment-9/warmup4.py
+++ week-9/assignment-9/warmup4.py
@@ -1,7 +1,7 @@
 import requests
-
+url = "https://thisurldoesnotexist.example.com"
 try:
-    response = requests.get("https://thisurldoesnotexist.example.com")
+    response = requests.get(url)
     if response.status_code != 200:
         print("Error: Could not reach the server. Check your connection and try again.")
     else:
