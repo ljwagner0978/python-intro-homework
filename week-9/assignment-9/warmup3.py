7c264ce7a62659e67d5cb2dac980e1969524cf61
ljwagner0978 <lswagner24@gmail.com>
2026-09-09 11:19:50 -0400

    Update warmup3.py with feedback from Michael

--- week-9/assignment-9/warmup3.py
+++ week-9/assignment-9/warmup3.py
@@ -6,16 +6,31 @@ API_KEY = config.api_key
 def fetch_european_countries():
     url = "https://api.restcountries.com/countries/v5?region=Europe"
     params = {
-        "api_key": API_KEY
+        "api_key": API_KEY,
+        "fields": "name,capital,region,population"
         }
+
     try:
         response = requests.get(url, params=params)
         response.raise_for_status()
         countries = response.json()
-        country_names = [col["names"]["common"] for col in countries["data"]["objects"]]
-        for name in country_names[:10]:
-            print(name)
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
+        return []
 
-fetch_european_countries()
+    cleaned = []
+    for country in countries["data"]["objects"]:
+        cleaned.append({
+            "name": country["names"]["common"],
+            "capital": country["capitals"][0]["name"] if country.get("capitals") else "N/A",
+            "region": country["region"],
+            "population": country["population"]
+        })
+    return cleaned
+
+def main():
+    country_names = fetch_european_countries()
+    for country in country_names[:10]:
+        print(country["name"])
+
+main()
